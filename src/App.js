cda973d88b1db9c68f32763ac473042fc6a93559
Ammar <ammar.n.sammour@gmail.com>
2019-08-05 18:33:27 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import ShoppingCart from './ShoppingCart'
 import Products from './Products';
+import Header from './Header'
 
 class App extends React.Component {
   render(){
   return (
     <div className="App">
+      <Header></Header>
       <Products/>
     </div>
   );
